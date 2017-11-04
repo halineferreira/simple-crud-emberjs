6b0168cae8fbefd1f53ae3ce80caa846f622adf3
Haline Ferreira <halineferreira@gmail.com>
2017-11-04 19:13:32 -0200

    Insert

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,5 +4,13 @@ export default Ember.Controller.extend({
     employees: [
         {nome: 'Marie Curie', email: 'haline@hehe.com'}, 
         {nome: 'Mae Jemison', email: 'haline@hehe.com'},
-        {nome: 'Albert Hofmann', email: 'haline@hehe.com'}]
+        {nome: 'Albert Hofmann', email: 'haline@hehe.com'}
+    ],
+    actions: {
+        addEmployee: function(){
+            let employeeName = this.get('employeeName');
+            let employeeEmail = this.get('employeeEmail');
+            this.get('employees').pushObject({nome: employeeName, email: employeeEmail})
+        }
+    }
 });
