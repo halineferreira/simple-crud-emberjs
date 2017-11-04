87c75d5329495c864eecbeaf2809208f6d9c5455
Haline Ferreira <halineferreira@gmail.com>
2017-11-04 18:44:02 -0200

    Changes for routing

---
+++ app/controllers/application.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+
+export default Ember.Controller.extend({
+    employees: [
+        {nome: 'Marie Curie', email: 'haline@hehe.com'}, 
+        {nome: 'Mae Jemison', email: 'haline@hehe.com'},
+        {nome: 'Albert Hofmann', email: 'haline@hehe.com'}]
+});
