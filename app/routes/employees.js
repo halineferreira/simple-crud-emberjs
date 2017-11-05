a186e9adde22715bfb42f0d6065e9e58645103d3
Haline Ferreira <halineferreira@gmail.com>
2017-11-05 10:57:16 -0200

    Improvements in employee-form

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -2,9 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return [
-            {nome: 'Marie Curie', email: 'haline@hehe.com'}, 
-            {nome: 'Mae Jemison', email: 'haline@hehe.com'},
-            {nome: 'Albert Hofmann', email: 'haline@hehe.com'}];
-      }
+        
+    }
 });
